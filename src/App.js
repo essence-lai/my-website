4212371dae5c5e060c2e56a274f367062fd38fbd
essence-lai <essence.alumni.ubc.ca>
2019-04-24 00:59:49 -0700

    setup with firebase

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          William says hi
         </a>
       </header>
     </div>
