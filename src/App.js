b820a9a7dd233629fc726a4710ee1bc214d2474b
essence-lai <essence.alumni.ubc.ca>
2019-07-03 15:39:19 -0700

    rearranged items to have resume included

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
-import reducers from './reducers/index'
-import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/home/index';
 import Contact from './components/contact/index';
 import ReallySmoothScroll from 'really-smooth-scroll';
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 ReallySmoothScroll.shim();
 
@@ -16,14 +11,12 @@ ReallySmoothScroll.shim();
 class App extends Component{
     render(){
         return (
-            <Provider store={createStoreWithMiddleware(reducers)}>
-                <Router>
-                    <div>
-                        <Route exact path="/" component={Home}/>
-                        <Route exact path="/contact" component={Contact}/>
-                    </div>
-                </Router>
-            </Provider>
+            <Router>
+                <div>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/contact" component={Contact}/>
+                </div>
+            </Router>
         );
 
     }
